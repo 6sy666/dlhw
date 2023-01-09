c5e5c5ec0eff6e6b464c0e83cb3eb9ea8315dff7
siyang liu <201300043@smail.nju.edu.cn>
2023-01-09 10:36:39 +0800

    up

--- run_scDeepCluster.py
+++ run_scDeepCluster.py
@@ -155,7 +155,7 @@ if __name__ == "__main__":
         ari = np.round(metrics.adjusted_rand_score(y, y_pred), 5)
         print('Evaluating cells: NMI= %.4f, ARI= %.4f' % (nmi, ari))
     final_latent_file=args.save_dir + args.final_latent_file+args.data_file
-    predict_label_file=args.predict_label_file+args.data_file
+    predict_label_file=args.save_dir +args.predict_label_file+args.data_file
     final_latent = model.encodeBatch(torch.tensor(adata.X, dtype=torch.float32)).cpu().numpy()
     np.savetxt(final_latent_file, final_latent, delimiter=",")
     np.savetxt(predict_label_file, y_pred, delimiter=",", fmt="%i")
