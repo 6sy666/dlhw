357e9185c866c70d96cce8135a74966a3907440c
siyang liu <201300043@smail.nju.edu.cn>
2023-01-09 10:25:04 +0800

    up

--- run_scDeepCluster.py
+++ run_scDeepCluster.py
@@ -58,9 +58,9 @@ if __name__ == "__main__":
                         help='file name to save final clustering labels')
     parser.add_argument('--device', default='cuda')
 
-
+    
     args = parser.parse_args()
-
+    
     data_mat = h5py.File(args.data_file, 'r')
     x = np.array(data_mat['X'])
     # y is the ground truth labels for evaluating clustering performance
@@ -154,8 +154,9 @@ if __name__ == "__main__":
         nmi = np.round(metrics.normalized_mutual_info_score(y, y_pred), 5)
         ari = np.round(metrics.adjusted_rand_score(y, y_pred), 5)
         print('Evaluating cells: NMI= %.4f, ARI= %.4f' % (nmi, ari))
-
+    final_latent_file=args.final_latent_file+args.data_file
+    predict_label_file=args.predict_label_file+args.data_file
     final_latent = model.encodeBatch(torch.tensor(adata.X, dtype=torch.float32)).cpu().numpy()
-    np.savetxt(args.final_latent_file, final_latent, delimiter=",")
-    np.savetxt(args.predict_label_file, y_pred, delimiter=",", fmt="%i")
+    np.savetxt(final_latent_file, final_latent, delimiter=",")
+    np.savetxt(predict_label_file, y_pred, delimiter=",", fmt="%i")
 
